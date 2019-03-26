fb837c2ca48ac9e15fa5cfc2f466560f66010d29
Yota Toyama <raviqqe@gmail.com>
2019-03-26 04:59:38 +0000

    Refactor Room.elm

--- src/Room.elm
+++ src/Room.elm
@@ -12,34 +12,31 @@ type alias Room =
 
 generate : ( Int, Int ) -> ( Int, Int ) -> Generator Room
 generate ( minX, maxX ) ( minY, maxY ) =
-    Random.map
-        (\( p1, p2 ) -> { leftTop = p1, rightBottom = p2 })
-        (Random.andThen
+    Position.generate ( minX, maxX - minWidth ) ( minY, maxY - minHeight )
+        |> Random.andThen
             (\position ->
                 Random.pair
                     (Random.constant position)
-                    (Position.generate
-                        ( Position.x position + minWidth, maxX )
-                        ( Position.y position + minHeight, maxY )
+                    (let
+                        ( x, y ) =
+                            position
+                     in
+                     Position.generate ( x + minWidth, maxX ) ( y + minHeight, maxY )
                     )
             )
-            (Position.generate ( minX, maxX - minWidth ) ( minY, maxY - minHeight ))
-        )
+        |> Random.map (\( p1, p2 ) -> { leftTop = p1, rightBottom = p2 })
 
 
 toPositions : Room -> Set ( Int, Int )
 toPositions room =
-    Set.fromList
-        (List.concat
-            (List.map
-                (\x ->
-                    List.map
-                        (\y -> ( x, y ))
-                        (List.range (Position.y room.leftTop) (Position.y room.rightBottom))
-                )
-                (List.range (Position.x room.leftTop) (Position.x room.rightBottom))
+    List.range (Position.x room.leftTop) (Position.x room.rightBottom)
+        |> List.map
+            (\x ->
+                List.range (Position.y room.leftTop) (Position.y room.rightBottom)
+                    |> List.map (\y -> ( x, y ))
             )
-        )
+        |> List.concat
+        |> Set.fromList
 
 
 minWidth : Int
